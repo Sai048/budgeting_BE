9fa5c7263796826b00b4bfae86f2fa00ef989a59
Sai_048 <saitejabellamkonda2002@gmail.com>
2025-10-27 20:26:08 +0530

    bugs fixed

--- apps/my-nest-app/src/app/app.module.ts
+++ apps/my-nest-app/src/app/app.module.ts
@@ -8,11 +8,9 @@ import { BudgetModule } from '../../../../libs/data/budget/src/lib/budget.module
 import { Transaction } from 'libs/data-models/budget-model/src/lib/budget.entity';
 import { BudgetExpectation } from 'libs/data-models/budgettExpectation-model/src/lib/budgettExpectation.entity';
 import { BudgetExpectationModule } from 'libs/data/budgetExpectation/src/lib/budgetExpectation.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env['DB-Host'],
